0fa7296b7326c3d0428ce65d29d3ff1291444bc1
pfredt1 <pfredt@gmail.com>
2022-01-17 20:33:20 -0500

    final project added

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+
+# tim = Turtle(shape="turtle")
+
 screen = Screen()
-
-def move_forwards():
-    tim.forward(10)
-
-def move_backwards():
-    tim.back(10)
-
-def turn_left():
-    tim.left(10)
-
-def turn_right():
-    tim.right(10)
-
-def clear():
-    tim.clear()
+x_start = -240
+y_start = -180
+colors = ["red", "orange", "black", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+turtles = []
+is_race_on = False
 
 
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet ", prompt="Which turtle will win the race? Enter a color: ")
 
+for turtle_index in range(0,6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(colors[turtle_index])
+    tim.goto(x=x_start, y=y_positions[turtle_index])
+    turtles.append(tim)
+
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for t in turtles:
+        rand_distance = random.randint(0, 30)
+        t.forward(rand_distance)
+        if t.xcor() >= 230:
+            is_race_on = False
+            if user_bet == t.pencolor():
+                print("You Win")
+            else:
+                print("you loose")
+
+
+
+
+
+
+
+
+
+# tim.goto(x_start,y_start)
 
 screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="a", fun=turn_left)
-screen.onkey(key="d", fun=turn_right)
-screen.onkey(key="c", fun=clear)
 
 screen.exitonclick()
